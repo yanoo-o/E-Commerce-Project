58d081aa2ef3ea58f653cf8b27367c3cc730b389
yanoo-o <87985090+yanoo-o@users.noreply.github.com>
2021-07-27 19:11:59 +0800

    Add files via upload

---
+++ lib/widgets/widget.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName:
+                Text(
+                  'Hi, Juan Dela Cruz',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+            accountEmail:
+                Text(
+                  'juandelacruz@gmail.com',
+                  style: TextStyle(color: Colors.white, fontSize: 12),
+                  textAlign: TextAlign.left,
+                ),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.blueGrey,
+              child: Image.network('https://icon-icons.com/icon/person/110935')
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.input),
+            title: Text('Welcome'),
+            onTap: () => {},
+          ),
+          ListTile(
+            leading: Icon(Icons.verified_user),
+            title: Text('Profile'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.border_color),
+            title: Text('Feedback'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+        ],
+      ),
+    );
+  }
+}
