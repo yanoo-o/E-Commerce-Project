fde6dd0237ad7caba486890da03ba55233f35a55
yanoo-o <87985090+yanoo-o@users.noreply.github.com>
2021-07-27 19:13:00 +0800

    Add files via upload

--- lib/market.dart
+++ lib/market.dart
@@ -2,6 +2,7 @@ import 'package:farmbili_2/main.dart';
 import 'package:flutter/material.dart';
 import 'package:farmbili_2/market.dart';
 import 'package:farmbili_2/Widgets/widget.dart';
+import 'package:farmbili_2/Widgets/search.dart';
 
 
 
@@ -43,59 +44,216 @@ class _MarketState extends State<Market> {
             onPressed: (){
             },
           ),
-          Align(
-            alignment: Alignment.topLeft,
-          ),
           ],
         ),
       drawer: NavDrawer(),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 2.0),
-        child: GridView.count(
-          crossAxisCount: 2,
-          padding: EdgeInsets.all(3.0),
+      body: SingleChildScrollView(
+        child: Column(
           children: <Widget>[
-            makeMarketItem("Promos and Vouchers", Icons.alarm),
-            makeMarketItem("Farm 1", Icons.alarm),
-            makeMarketItem("Farm 2", Icons.alarm),
-            makeMarketItem("Farm 3", Icons.alarm),
-            makeMarketItem("Farm 4", Icons.alarm),
-            makeMarketItem("Farm 5", Icons.alarm),
-          ],
-        ),
+            SizedBox(height:20),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(' What would you like to buy?', style: TextStyle(
+                color: Color(0xFF3a3737),
+                fontSize: 25,
+                fontWeight: FontWeight.w500,),
+              ),
+            ),
+            SizedBox(height:10),
+            SearchWidget(),
+            SizedBox(height:10),
+            Container(
+              height:200.0,
+              width: 500.0,
+              child:Align(
+                alignment: Alignment(-0.9, 0.8),
+                child: Text(
+                'Promos & Vouchers',
+                style: TextStyle(
+                  fontSize: 20, color: Colors.white,
+                ),
+              ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
+                image: DecorationImage(
+                  image: AssetImage('assets/images/promo.png'),
+                  fit: BoxFit.contain,
+                  alignment: Alignment.centerRight,
+                ),
+                borderRadius: BorderRadius.circular(10),
+              )
+            ),
+            SizedBox(height:10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+              Container(
+                height: 200.0,
+                width: 250.0,
+                  child:Align(
+                    alignment: Alignment(-0.9, 0.8),
+                    child: Text(
+                      'Delivery',
+                      style: TextStyle(
+                        fontSize: 20, color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.brown,
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/veggies.png'),
+                      scale: 0.8,
+                      alignment: Alignment.topRight,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  )
+              ),
+                Container(
+                    height: 200.0,
+                    width: 250.0,
+                    child:Align(
+                      alignment: Alignment(-0.9, 0.8),
+                      child: Text(
+                        'Pick-Up',
+                        style: TextStyle(
+                          fontSize: 20, color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.lightGreen,
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/market.png'),
+                        scale: 0.4,
+                        alignment: Alignment.bottomRight,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    )
+                )
+            ]
+            ),
+            SizedBox(height:30),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text('  Your Shops', style: TextStyle(
+                color: Color(0xFF3a3737),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,),
+              ),
+            ),
+            SizedBox(height:10),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      height: 175.0,
+                      width: 250.0,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        image: new DecorationImage(
+                          image: new AssetImage('assets/images/bg.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child:Align(
+                        alignment: Alignment(-0.8, 0.9),
+                        child: Text(
+                          'Dela Cruz Farm',
+                          style: TextStyle(
+                            fontSize: 15, color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+
+                    ),
+                    SizedBox(width:10),
+                    Container(
+                      height: 175.0,
+                      width: 250.0,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        image: new DecorationImage(
+                          image: new AssetImage('assets/images/commonwealth.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child:Align(
+                        alignment: Alignment(-0.8, 0.9),
+                        child: Text(
+                          'Antipolo Farm',
+                          style: TextStyle(
+                            fontSize: 15, color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+
+                    ),
+                    SizedBox(width:10),
+                    Container(
+                      height: 175.0,
+                      width: 250.0,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        image: new DecorationImage(
+                          image: new AssetImage('assets/images/bg.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child:Align(
+                        alignment: Alignment(-0.8, 0.9),
+                        child: Text(
+                          'Commonwealth Market',
+                          style: TextStyle(
+                            fontSize: 15, color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+
+                    ),
+                    SizedBox(width:10),
+                    Container(
+                      height: 175.0,
+                      width: 250.0,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        image: new DecorationImage(
+                          image: new AssetImage('assets/images/commonwealth.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child:Align(
+                        alignment: Alignment(-0.8, 0.9),
+                        child: Text(
+                          'Balintawak Market',
+                          style: TextStyle(
+                            fontSize: 15, color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+
+                    ),
+                  ]
+
+              ),
+            ),
+
+          ]
+        )
       ),
     );
   }
 }
 
-Card makeMarketItem(String title, IconData icon) {
-  return Card(
-      elevation: 1.0,
-      margin: new EdgeInsets.all(8.0),
-      child: Container(
-        decoration: BoxDecoration(color: Color.fromRGBO(220, 220, 220, 1.0)),
-        child: new InkWell(
-          onTap: () {},
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisSize: MainAxisSize.min,
-            verticalDirection: VerticalDirection.down,
-            children: <Widget>[
-              SizedBox(height: 50.0),
-              Center(
-                  child: Icon(
-                    icon,
-                    size: 40.0,
-                    color: Colors.black,
-                  )),
-              SizedBox(height: 20.0),
-              new Center(
-                child: new Text(title,
-                    style:
-                    new TextStyle(fontSize: 18.0, color: Colors.black)),
-              )
-            ],
-          ),
-        ),
-      ));
-}
+
